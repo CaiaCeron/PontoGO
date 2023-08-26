60f1cf28e61ce86e049ebc8f407815e2bcca8eae
WIN-L71F8PMAL85\Caia <caiaceron@gmail.com>
2023-08-26 18:10:25 -0300

    First Commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Login() {
+  return (
+    <>
+      <h1>Dashboard Page</h1>
+      <Link href="/"> Go back to the Landing page</Link>
+    </>
+  );
+}
